3422b2d0ed8e1b7ed2b8c6097c29c8db19be5513
mert özsoy <40569187+mrtozy@users.noreply.github.com>
2022-09-23 18:33:28 +0300

    Add files via upload

---
+++ ReCapDemoClasses/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dorthesap = new DortIslem();
+
+        int sonuc = dorthesap.Carp(3, 4);
+        System.out.println(sonuc);
+
+        int sonuc2 = dorthesap.Bol(89, 3);
+        System.out.println(sonuc2);
+
+	}
+
+}
